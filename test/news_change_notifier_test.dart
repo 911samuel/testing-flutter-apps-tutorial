8ed52729675066ee306d3dcf0483f5c9b48401f6
911samuel <abayizeraeaz@gmail.com>
2025-06-19 16:39:03 +0200

    apply unit testing to the articles

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -18,12 +18,55 @@ class BadMockNewsService implements NewsService {
   }
 }
 
-class MockNewsService extends Mock implements NewsService{}
+class MockNewsService extends Mock implements NewsService {}
 
 void main() {
   late NewsChangeNotifier sut;
+  late MockNewsService mockNewsService;
 
   setUp(() {
-    sut = NewsChangeNotifier(BadMockNewsService as NewsService);
+    mockNewsService = MockNewsService();
+    sut = NewsChangeNotifier(mockNewsService);
+  });
+
+  test('initial values are correct', () {
+    expect(sut.articles, []);
+    expect(sut.isLoading, false);
+  });
+
+  group('get articles', () {
+    final articlesFromService = [
+      Article(title: 'test 1', content: 'test 1 content'),
+      Article(title: 'test 2', content: 'test 2 content'),
+      Article(title: 'test 3', content: 'test 3 content'),
+      Article(title: 'test 4', content: 'test 4 content'),
+    ];
+
+    void arrangeNewArticlesReturn3Articles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((_) async => articlesFromService);
+    }
+
+    test(
+      "gets Articles using NewsService",
+      () async {
+        arrangeNewArticlesReturn3Articles();
+        await sut.getArticles();
+        verify(() => mockNewsService.getArticles()).called(1);
+      },
+    );
+
+    test(
+      "indicates loading of data, sets articles to the ones from service, indicates that data is not loading anymore",
+      () async {
+        arrangeNewArticlesReturn3Articles();
+        final future = sut.getArticles();
+        expect(sut.isLoading, true);
+        await future;
+        expect(sut.articles, articlesFromService);
+        expect(sut.isLoading, false);
+
+      },
+    );
   });
 }
